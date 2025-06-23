a97a6e8cb005c99933b8707430af7acd0bc5ced2
Gabriel Ribas Ferreira de Almeida <gabrielribasfal@gmail.com>
2025-06-23 14:50:41 -0300

    feat: adicionando validação das placas no db

--- backend/index.ts
+++ backend/index.ts
@@ -12,14 +12,6 @@ app.use(express.json())
 const port = 3000;
 connect().then(db => api(db)).catch(err=>console.error("Falha ao conectar no mysql", err));
 
-
-// function validarNomeUsuario(req, res, next) {
-//   const { name } = req.body
-//   if (!name || typeof name !=='string' || name.trim() === '') {
-//     return res.status(400).json({erro: "O campo 'name' é obrigatório e deve ser uma string válida"})
-//   }
-//   next()
-
 function api(db:Connection) {
   const userRepository = new UserRepository(db)
   const placaRepository = new PlacaRepository(db)
